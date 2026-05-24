3f21c2c98415a06b1bed25924b17c664988724be
Huy Bao Nguyen <jimmyhuybao@gmail.com>
2026-05-24 13:03:04 +0200

    added account, wallet, log in, sign up pages. added drop-down bars for languages and user icon

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, request
 
 app = Flask(__name__)
+app.secret_key = 'buuro'
+
+@app.route('/set-language/<lang>')
+def set_language(lang):
+    session['lang'] = lang
+    return redirect(request.referrer or url_for('home'))
 
 @app.route('/')
 def home():
@@ -15,7 +21,23 @@ def events():
     return render_template('events.html')
 
 @app.route('/requests')
-def requests():
+def requests_page():
     return render_template('requests.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/wallet')
+def wallet():
+    return render_template('wallet.html')
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
 if __name__ == '__main__':
     app.run(debug=True)
